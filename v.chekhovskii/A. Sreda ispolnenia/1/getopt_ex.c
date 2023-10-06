4abe4591a1dbf16bc218bfad5239ce5479ac5872
bakafox <v.chekhovskii@g.nsu.ru>
2023-10-06 12:23:51 +0700

    Лаба 1 (незаконченная)

--- v.chekhovskii/A. Sreda ispolnenia/1/getopt_ex.c
+++ v.chekhovskii/A. Sreda ispolnenia/1/getopt_ex.c
@@ -1,36 +1,55 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 main(int argc, char *argv[]) {
-    char options[] = "f:dg:"; /* valid options */
-    int c, invalid = 0, dflg = 0, fflg = 0, gflg = 0;
-    char *f_ptr, *g_ptr;
-
-    printf("argc equals %d\n", argc);
+    char options[] = "ispuU:cC:dvV:h";  // valid options
+    int c = 0;
+    //printf("argc equals %d\n", argc);
 
     while ((c = getopt(argc, argv, options)) != EOF) {
         switch (c) {
+        case 'i':
+            printf("Real user id: %d\n", getuid());
+            printf("Effective user id: %d\n", geteuid());
+            printf("Real group id: %d\n", getgid());
+            printf("Effective group id: %d\n", getegid());
+        case 's':
+            setpgid(0, 0);
+            break;
+        case 'p':
+            //system("ps a");
+            printf("Process id: %d\n", getpid());
+            printf("Parent id: %d\n", getppid());
+            printf("Group id: %d\n", getpgrp());
+            break;
+        case 'u':
+        case 'U':
+        case 'c':
+        case 'C':
         case 'd':
-            dflg++;
+        case 'v':
+        case 'V':
+            printf("Work in progress!\n");
             break;
-        case 'f':
-            fflg++;
-            f_ptr = optarg;
+
+        case 'h':
+            printf("-i :: Prints real and effective user and group IDs.\n");
+            printf("-s :: Process becomes the group leader.\n");
+            printf("-p :: Prints the process, parent process, and process group identifiers.\n");
+            printf("-u :: Prints the value of ulimit.\n");
+            printf("-Unew_ulimit :: Changes the value of ulimit.\n");
+            printf("-c :: Prints the size in bytes of the core file that can be created.\n");
+            printf("-Csize :: Changes the size of the core file.\n");
+            printf("-d :: Prints the current working directory.\n");
+            printf("-v :: Prints environment variables and their values.\n");
+            printf("-Vname=value :: Introduces a new variable into the environment or changes the value of an existing variable.\n");
+            printf("-h :: Prints this help message.\n");
             break;
-        case 'g':
-            gflg++;
-            g_ptr = optarg;
-            break;
-        case '?':
-            printf("invalid option is %c\n", optopt);
-            invalid++;
+        case ':':
+            printf("Option '%c' requires an argument.\n", optopt);
+        default:
+            printf("Option '%c' is invalid; use 'h' for help.\n", optopt);
         }
     }
-
-    printf("dflg equals %d\n", dflg);
-    if (fflg) { printf("f_ptr points to %s\n", f_ptr); }
-    if (gflg) { printf("g_ptr points to %s\n", g_ptr); }
-    printf("invalid equals %d\n", invalid);
-    printf("optind equals %d\n", optind);
-    if (optind < argc) { printf("next parameter = %s\n", argv[optind]); }
 }
