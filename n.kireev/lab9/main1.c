52db77852663824d6251788b609812abe40630a9
evrey1917 <143984979+evrey1917@users.noreply.github.com>
2023-10-27 12:00:57 +0700

    Create main1.c

---
+++ n.kireev/lab9/main1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main()
+{
+    printf("\nid_main: %d\nid_par_main: %d\n\n", getpid(), getppid());
+    pid_t sub_proc = fork();
+    printf("\nid_1: %d -- id_2: %d -- sub_proc: %d\n\n", getpid(), getppid(), sub_proc);
+    int status;
+
+    if (sub_proc == -1)
+    {
+        perror("Failed sub-processing");
+        _exit(1);
+    }
+
+    if (sub_proc == 0)       // 0, if we in sub-process
+    {
+        execl("/bin/cat", "cat", "some.txt", NULL);
+        _exit(EXIT_FAILURE);
+    }
+    else
+    {
+        printf("\n\nummm...i'm your parent... uuuh...\n");
+    }
+}
